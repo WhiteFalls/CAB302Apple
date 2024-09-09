775cb13616e96bb64c5c9b1c5269b59f56444c4e
WhiteFalls <liam@mailserv.com.au>
2024-09-09 14:38:37 +1000

    KAN-6 Account Registration Started.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.gardenplanner {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.gardenplanner to javafx.fxml;
     exports com.example.gardenplanner;
+    exports com.register.gardenplanner;
+    opens com.register.gardenplanner to javafx.fxml;
 }
