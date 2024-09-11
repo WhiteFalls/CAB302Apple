47d89d7cbcbfab9009ca9ac46e5f184df208e64d
Brian1152 <n11541555@qut.edu.au>
2024-09-11 18:10:30 +1000

    created main page

---
+++ src/main/java/com/example/gardenplanner/Tasks/MainPage.java
@@ -0,0 +1,16 @@
+package com.example.gardenplanner.Tasks;
+
+import com.example.gardenplanner.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+
+public class MainPage {
+    @FXML
+    protected void nextbuttinclick() throws I0Exception {
+        Stage stage = (Stage) nextbutton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication).class.getResource()
+    }
+
+}
