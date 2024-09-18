2451188feeddefbef176fadfa79df79979529a35
Brian1152 <n11541555@qut.edu.au>
2024-09-18 16:51:08 +1000

    Fixed main-page.fxml

--- src/main/java/IPerson.java
+++ src/main/java/IPerson.java
@@ -1,3 +1,5 @@
+import People.IPerson;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
---
+++ src/main/java/com/example/gardenplanner/controller/MainPage.java
@@ -0,0 +1,49 @@
+package com.example.gardenplanner.controller;
+
+import com.example.gardenplanner.GardenManagementController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+
+
+public class MainPage {
+    @FXML
+    private Button UpdatesButton;
+
+    @FXML
+    protected void onUpdatesButtonClick() throws IOException {
+        Stage stage = (Stage) UpdatesButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(GardenManagementController.class.getResource("garden-management.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 600);
+        stage.setScene(scene);
+    }
+
+//    @FXML
+//    protected void OnNextButtonClick("Garden") throws IOException {
+//        Stage stage = (Stage) nextbutton.getScene().getWindow();
+//        FXMLLoader fxmlLoader = new FXMLLoader(. class.getResource("usertodoList.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(),.WIDTH, .HEIGHT);
+//        stage.setScene(scene);
+//    }
+//
+//    @FXML
+//    protected void OnNextButtonClick("Settings") throws IOException {
+//        Stage stage = (Stage) onNextbutton.getScene().getWindow();
+//        FXMLLoader fxmlLoader = new FXMLLoader(. class.getResource("settings.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(),.WIDTH, .HEIGHT);
+//        stage.setScene(scene);
+//    }
+//
+//    @FXML
+//    protected void OnNextButtonClick("Settings") throws IOException {
+//        Stage stage = (Stage) onNextbutton.getScene().getWindow();
+//        FXMLLoader fxmlLoader = new FXMLLoader(. class.getResource(".fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(),.WIDTH, .HEIGHT);
+//        stage.setScene(scene);
+//    }
+}
+
