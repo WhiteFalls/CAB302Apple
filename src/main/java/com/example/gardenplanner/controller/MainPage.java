04c849df8db33c6f3ee6b3a387e8f4924be48197
HuanNguyen-Dev <hnguy138@outlook.com>
2024-09-23 19:23:10 +1000

    Added a method to remove users own garden, it toggles with add garden, everything functional

--- src/main/java/com/example/gardenplanner/controller/MainPage.java
+++ src/main/java/com/example/gardenplanner/controller/MainPage.java
@@ -8,6 +8,8 @@ import People.IPerson;
 import com.example.gardenplanner.HelloApplication;
 import com.example.gardenplanner.UserSession;
 import com.example.gardenplanner.controller.GardenManagementController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -22,6 +24,9 @@ import java.util.Optional;
 public class MainPage {
     @FXML
     private Button UpdatesButton;
+
+    @FXML
+    private Button addGarden;
 
     private UserSession currentUser = UserSession.getInstance();  // can be used to refactor initialize
     private GardenDAO gardenDAO;
@@ -44,6 +49,10 @@ public class MainPage {
         this.loggedInUser = personDAO.getPerson(personId);
 
         gardenDAO = new GardenDAO(); // gardens can be created on main page
+        int gardensOwned = findNumGardensOwned();
+        if (gardensOwned == 1){
+            setButtonToRemove();
+        }
     }
 
     /**
@@ -78,7 +87,7 @@ public class MainPage {
 
 
     @FXML
-    protected void addGarden() throws IOException, SQLException {
+    protected void addGarden() throws SQLException {
         // this code wil be needed in the future to swap to the actual garden plot
 //        Stage stage = (Stage) UpdatesButton.getScene().getWindow();
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("garden-plot.fxml"));
@@ -88,6 +97,8 @@ public class MainPage {
             int gardensOwned = findNumGardensOwned();
             if (gardensOwned >= 1){
                 displayErrorPopup("You are only allowed to own one garden!");
+                // change button to remove
+                setButtonToRemove();
             }
             else if (gardensOwned == 0){
                 // for now, we just create a new garden in the database
@@ -97,9 +108,14 @@ public class MainPage {
                 dialog.setContentText("Please enter your garden name:");
 
                 Optional<String> result = dialog.showAndWait();
-                if (result.isPresent()) {
-                    Garden garden = new Garden(currentUser.getPersonId(),result.get()); // currently, garden name will be users name
+                // the result.get.trim is needed as putting a name with a space actually destroys the database :(
+                if (result.isPresent() && !result.get().isEmpty() && result.get().trim().length() > 1) { // holy salad
+                    Garden garden = new Garden(currentUser.getPersonId(),result.get());
                     gardenDAO.addGarden(garden);
+                    setButtonToRemove();
+                }
+                else{
+                    displayErrorPopup("Garden name must include a character!");
                 }
             }
             else{
@@ -107,6 +123,50 @@ public class MainPage {
                 displayUnknownError();
             }
     }
+
+    private void setButtonToAdd() {
+        addGarden.setText("Add Garden");
+        addGarden.setOnAction(e -> {
+            try {
+                addGarden();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    private void setButtonToRemove() {
+        addGarden.setText("Remove Garden");
+        addGarden.setOnAction(e -> handleRemoveGardenButton(gardenDAO.getGardenByUserId(loggedInUser.getUserId())));
+    }
+
+    private void handleRemoveGardenButton(Garden garden){
+        gardenDAO.deleteGarden(garden.getGardenId());
+        int numGardens = findNumGardensOwned(); // check if garden is removed from database
+        System.out.println(numGardens) ;
+        if (numGardens == 0) // only allowed to own one garden
+        {
+            displayPopup("Garden successfully removed!");
+            addGarden.setText("Add Garden");
+            addGarden.setOnAction(e -> {
+                try {
+                    setButtonToAdd();
+                    addGarden();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+
+        }
+        else if (numGardens == 1){
+            displayErrorPopup("Garden has not been removed, please try again!");
+        }
+        else{
+            displayUnknownError();
+        }
+
+    }
+
 
     private int findNumGardensOwned(){
         String query = "SELECT COUNT(*) FROM Gardens WHERE garden_owner = ?";
@@ -136,6 +196,32 @@ public class MainPage {
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    // stolen from gardenmanagement controller - we really need a utlis class
+    private void displayPopup(String message)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    // stolen from gardenmanagement controller - we really need a utlis class
+    private boolean displayConfirmPopup(String message)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("CONFIRMATION");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK)
+        {
+            return true;
+        }
+        return false;
     }
 
 
