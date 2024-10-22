cefcf3e0b5f432f3fdfd173fe99c43edd0d0c599
Brian1152 <n11541555@qut.edu.au>
2024-10-22 15:55:07 +1000

    merge garden planner changes

--- src/main/java/com/example/gardenplanner/HelloApplication.java
+++ src/main/java/com/example/gardenplanner/HelloApplication.java
@@ -20,7 +20,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         checkAndCreateDatabase();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("plant-info-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 600);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setTitle("Garden Manager");
