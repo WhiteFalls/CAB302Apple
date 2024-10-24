b5e3c395b36fe99b3062bc8a039fd472d9390dc2
Darragh-Nugent <darraghnugent312@gmail.com>
2024-10-24 22:03:17 +1000

    Wrote JavaDoc comments for IPlantDAO

--- src/main/java/Database/IPlantDAO.java
+++ src/main/java/Database/IPlantDAO.java
@@ -8,7 +8,18 @@ public interface IPlantDAO {
 
 
     public Plant getPlantById(Plant plant);
+
+    /**
+     * Gets all the plants in the database
+     * @return An ArrayList of all the plants in the database
+     */
     public ArrayList<Plant> getAllPlants();
+
+    /**
+     * Gets all that plants that contain a specified name/word
+     * @param userInput The word that the plants need to contain
+     * @return An ArrayList of all the plants that contain that word
+     */
     public ArrayList<Plant> getPlantContainsName(String userInput);
     Plant getPlantById(int plantId);
 }
