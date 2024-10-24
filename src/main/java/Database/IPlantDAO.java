709c6ed2f6ca155c27865d7a27cc691d6c4c7cdd
Darragh-Nugent <darraghnugent312@gmail.com>
2024-10-24 21:20:31 +1000

    added additional methods to IPlantDAO

--- src/main/java/Database/IPlantDAO.java
+++ src/main/java/Database/IPlantDAO.java
@@ -1,12 +1,15 @@
 package Database;
 
 import GardenCell.Plant;
+
+import java.util.ArrayList;
 import java.util.List;
 public interface IPlantDAO {
 
 
     public Plant getPlantById(Plant plant);
-
+    public ArrayList<Plant> getAllPlants();
+    public ArrayList<Plant> getPlantContainsName(String userInput);
     Plant getPlantById(int plantId);
 }
 
