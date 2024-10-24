3689abf9ac8d33943ec2d05492156d3c7b3e8d5a
Darragh-Nugent <darraghnugent312@gmail.com>
2024-10-24 22:10:44 +1000

    Wrote JavaDoc comments for IGardenMapDAO

--- src/main/java/Database/IGardenMapDAO.java
+++ src/main/java/Database/IGardenMapDAO.java
@@ -4,11 +4,31 @@ import GardenCell.GardenCell;
 import GardenCell.Garden;
 
 public interface IGardenMapDAO {
+
+    /**
+     * Creates a default map for when the person first generates their garden
+     * @param garden The newly generated garden
+     */
     public void createDefaultMap(Garden garden);
 
+    /**
+     * Gets all the cells in the garden formated into their appropriate rows and columns
+     * @param garden The garden to get the cells from
+     * @return A 2D array of all the cells in the garden
+     */
     public GardenCell[][] getGardenCells(Garden garden);
 
+    /**
+     * Updates an individual cell in the garden
+     * @param cell The new cell with the same x and y coordinates as the old cell
+     */
     public void updateCell(GardenCell cell);
 
+    /**
+     * Resizes the garden to the specified size
+     * @param garden The garden to be resized
+     * @param newWidth The new width of the garden
+     * @param newHeight The new height of the garden
+     */
     public void resizeMap(Garden garden, int newWidth, int newHeight);
 }
